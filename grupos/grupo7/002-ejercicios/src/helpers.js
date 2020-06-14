a6fa0d30f283268db454acde82d41ef3567d5174
Juan Calou <juan.calou@patagonian.it>
2020-06-14 18:18:53 -0300

    ejercicio 2 terminado y testeado

--- grupos/grupo7/002-ejercicios/src/helpers.js
+++ grupos/grupo7/002-ejercicios/src/helpers.js
@@ -1,9 +1,10 @@
 // 6) Mover helpers y el todo el co´digo a un módulo, creando un nuevo archivo helpers.js
 import { database } from './baseDeDatos';
+const getById = (tabla) => (id) => database[tabla].find((i) => i.id === id);
+const getByNombre = (tabla) => (nombre) => database[tabla].find((i) => i.nombre === nombre);
 
 // 2) Implementar una función que obtenga una universidad por Id
 // 🤓 Tip: pueden ir probando las funciones usando console.log() asegurándose que los resultados sean los esperados
-const getById = (tabla) => (id) => database[tabla].find((i) => i.id === id);
 const getUniversidadById = getById('universidades');
 
 // 3) Implementar una función que obtenga un profesor por Id
@@ -11,13 +12,26 @@ const getProfesorById = getById('profesores');
 
 // 4) Implementar una función que obtenga una materia por Id
 // 🤓 Tip: Comparar con la función del ejercicio (3) y ver si se les ocurre una función genérica que sirva para cualquier tabla
-const getMateriasById = getById('materias');
+const getMateriaById = getById('materias');
+
+const getAlumnoByNombre = getByNombre('alumnos');
+const getMateriaByNombre = getByNombre('materias');
+
+// 7) Crear un nuevo método en helpers que devuelva el último ID utilizado en una tabla
+const lastID = (table) => {
+    let max = 0;
+    table.map(e => {
+        if (e.id > max) max = e.id;
+    });
+    return max;
+}
 
 // 5) Crear un objeto 'helpers' que contenga las funciones como métodos
-const helpers = {
+export const helpers = {
     getUniversidadById,
     getProfesorById,
-    getMateriasById
+    getMateriaById,
+    getAlumnoByNombre,
+    getMateriaByNombre,
+    lastID
 };
-
-export default helpers;
--- grupos/grupo7/002-ejercicios/src/index.js
+++ grupos/grupo7/002-ejercicios/src/index.js
@@ -1,25 +1,47 @@
 // 1) Importar el objeto 'database' del archivo "./basededatos"
 import { database } from './baseDeDatos';
-import { helpers } from './helpers';
-
-// 7) Crear un nuevo método en helpers que devuelva el último ID utilizado en una tabla
-const lastID = (el) => Object.keys(el).pop().id,
-helpers.push(lastID);
 
 // 8) Importar helpers desde su propio módulo
+import { helpers } from './helpers';
 
 // 9) Implementar una función que permite insertar una nueva provincia en la base de datos
 //    La función tomará como parámetro el nombre de la provincia y devolverá el ID de la nueva provincia
 // 🤓 Tip: Reusar una o más funciones de helper
-const insertarProvincia = (nombreProvincia) => {
-    let id = helpers.lastID(el) + 1;
-    database.provincias = [...database.provincias, { id: id, nombre: nombreProvincia }];
-    return id;
+export const insertarProvincia = (nombreProvincia) => {
+    const newID = helpers.lastID(database.provincias) + 1;
+    database.provincias = [...database.provincias, { id: newID, nombre: nombreProvincia }];
+    return newID;
 }
+export const insertarAlumno = (nombre, edad, provincia) => {
+    const newID = helpers.lastID(database.alumnos) + 1;
+    database.alumnos = [...database.alumnos, { id: newID, nombre, edad, provincia }];
+    return newID;
+}
+export const insertarMateria = (nombre, profesores, universidad) => {
+    const newID = helpers.lastID(database.materias) + 1;
+    database.materias = [...database.materias, { id: newID, nombre, profesores, universidad }];
+    return newID;
+}
+// console.log(insertarProvincia('nueva'));
+// console.log(database.provincias);
 
-// 10) Implementar una función que reciba el id de una materia y devuelva la materia son los ids de universidad y profesores resueltos a sus nombres
+// 10) Implementar una función que reciba el id de una materia y devuelva la materia
+//     con los ids de universidad y profesores resueltos a sus nombres
+export const datosMateria = (idMateria) => {
+    const mat = helpers.getMateriaById(idMateria);
+    const materia = {
+        id: mat.id,
+        nombre: mat.nombre,
+        profesores: mat.profesores.map((e) => helpers.getProfesorById(e).nombre),
+        universidad: helpers.getUniversidadById(mat.universidad).nombre
+    };
 
-// 11) Implementar una función que muestre en consola la información para todos los alumnos de la siguiente manera:
+    return materia;
+}
+//console.log(datosMateria(1));
+
+// 11) Implementar una función que muestre en consola la información para todos los 
+//      alumnos de la siguiente manera:
 // NOTAS DE ALUMNOS
 // ----------------
 // RIGOBERTO MANCHU        <-- En mayúsculas
@@ -27,7 +49,50 @@ const insertarProvincia = (nombreProvincia) => {
 // ....
 // ALUMNO 2
 // ...
+export const infoAlumnos = () => {
+    let resultText = 'NOTAS DE ALUMNOS\n' + '----------------\n';
+    
+    database.alumnos.map((a) => {
+        resultText = resultText + a.nombre.toUpperCase() + '\n';
+        database.calificaciones
+            .filter((c) => c.alumno === a.id)
+            .map((ca) => {
+                resultText = resultText + helpers.getMateriaById(ca.materia).nombre + ': ' + ca.nota + '\n';
+            });
+    });
+
+    return resultText;
+}
+//console.log(infoAlumnos());
 
 // 12) Implementar una función que guarde la calificación de un alumno y una materia
 //     La función recibirá: 'nombre del alumno', 'nombre de la materia', 'nota'
 //     Si el alumno y/o la materia no existen deberán crearlos en sus respectivas tablas
+export const nuevaCalificacion = (nombreAlumno, nombreMateria, nota) => {
+    let idAlumno, idMateria;
+    
+    if(helpers.getAlumnoByNombre(nombreAlumno)?.id) {
+        idAlumno = helpers.getAlumnoByNombre(nombreAlumno).id;
+    } else {
+        idAlumno = insertarAlumno(nombreAlumno, 0, 0);
+    }
+
+    if(helpers.getMateriaByNombre(nombreMateria)?.id) {
+        idMateria = helpers.getMateriaByNombre(nombreMateria).id;
+    } else {
+        idMateria = insertarMateria(nombreMateria, [], 0);
+    }
+
+    database.calificaciones = [
+        ...database.calificaciones, 
+        { 
+            alumno: idAlumno, 
+            materia: idMateria,
+            nota
+        }
+    ];
+    return '';
+}
+// console.log(nuevaCalificacion('Juan', 'Materianueva', 10));
+// console.log(nuevaCalificacion('Suzana Mendez', 'Materianueva', 10));
+// console.log(database.calificaciones);
