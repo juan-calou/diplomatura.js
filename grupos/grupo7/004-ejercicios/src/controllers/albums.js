51ab8d78d6224af54b482064e1dc113a180a9f08
Romina <romina.bertini@grupocapsa.com.ar>
2020-07-10 13:11:33 -0300

    Ejercicio 004 punto 3 - visualización de datos

--- grupos/grupo7/004-ejercicios/src/controllers/albums.js
+++ grupos/grupo7/004-ejercicios/src/controllers/albums.js
@@ -4,31 +4,33 @@ import fetch from 'node-fetch';
 var albumsApi = express.Router();
 
 albumsApi.get('/', async function (req, res) {
-    const albums = await getAlbums();
-    const users = await fetch('https://jsonplaceholder.typicode.com/users').then(value => value.json());
-    let rta = albums.map(obj => {return{id: obj.id, title: obj.title, user: users.find(e=>e.id === obj.userId)};});
+    const albums = await fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(a=>a.json());
+    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(u=>u.json());
+    const rta = await albums.map(obj => {
+        return {
+            id: obj.id, 
+            title: obj.title, 
+            user: users.find(e=>e.id === obj.userId)
+        };
+    });
     //console.log(rta);
-    res.send('<p>Albums: </p>' + rta);
+    //res.send('<p>Albums: <br></p> <pre>' + rta + '</pre>');
+    res.send(rta);
 });
 
 albumsApi.get('/:id', async function (req, res) {
-    const album = await getAlbums(req.params.id);
-    const phot = await fetch('https://jsonplaceholder.typicode.com/albums/' + req.params.id + '/photos').then(value => value.json());
+    let album = await fetch('https://jsonplaceholder.typicode.com/albums/' + req.params.id)
+        .then((value) => value.json())
+        .catch((e) => res.send("Error: " + e));
+    const phot = await fetch('https://jsonplaceholder.typicode.com/albums/' + req.params.id + '/photos')
+        .then((value) => value.json())
+        .catch((e) => res.send("Error: " + e));
     album.photos = phot.map(e=>{return {id: e.id, title: e.title, url: e.url, thumbnailUrl: e.thumbnailUrl};});
     //console.log(album);
     //res.send(`Hola album ${req.params.id}.`);
-    res.send('<p>Albums: </p>' + album);
-});
-
-function getAlbums (id){
-    if (id)
-        return fetch('https://jsonplaceholder.typicode.com/albums/' + id)
-                    .then((value) => value.json())
-                    .catch((e) => res.send("Error: " + e));
-    else        
-        return fetch('https://jsonplaceholder.typicode.com/albums')
-            .then((value) => value.json())
-            .catch((e) => res.send("Error: " + e));
-}
+    res.send(album);
+}); 
 
 export default albumsApi;
