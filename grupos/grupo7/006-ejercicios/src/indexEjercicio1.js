a0af4b82fb45fa7b3e85e1fc759e3507966710b9
Romina <romina.bertini@grupocapsa.com.ar>
2020-07-23 18:39:56 -0300

    Ejercicio1 y principio del 2

---
+++ grupos/grupo7/006-ejercicios/src/indexEjercicio1.js
@@ -0,0 +1,34 @@
+const MOUNTAINS = [
+    { name: 'Kilimanjaro', height: 5895, place: 'Tanzania' },
+    { name: 'Everest', height: 8848, place: 'Nepal' },
+    { name: 'Mount Fuji', height: 3776, place: 'Japan' },
+    { name: 'Vaalserberg', height: 323, place: 'Netherlands' },
+    { name: 'Denali', height: 6168, place: 'United States' },
+    { name: 'Popocatepetl', height: 5465, place: 'Mexico' },
+    { name: 'Mont Blanc', height: 4808, place: 'Italy/France' },
+  ];
+
+const createCell = (contenido, tipo) => {
+    let celda = document.createElement(tipo);
+    const texto = document.createTextNode(contenido);
+    celda.appendChild(texto);
+    return celda;
+};
+
+const createRow = (data = [],tipo = "td") => {
+    let fila = document.createElement("tr");
+    data.forEach(element => {
+        fila.appendChild(createCell(element,tipo));
+    });
+    return fila;
+};
+
+let nuevTabla = document.createElement("table");
+nuevTabla.appendChild(createRow(["name","height","place"],"th"));
+MOUNTAINS.forEach(element => {
+    nuevTabla.appendChild(createRow([element.name,element.height,element.place]));
+});
+document.getElementById('app').appendChild(nuevTabla);
+
+
+
